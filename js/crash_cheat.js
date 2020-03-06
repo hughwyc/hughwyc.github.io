8bee0df777253e96f57579c97efdd55bca090670
hughwyc <hugh.wyc@gmail.com>
2020-03-07 02:26:24 +0800

    Site updated: 2020-03-07 02:26:23

--- js/crash_cheat.js
+++ js/crash_cheat.js
@@ -4,12 +4,12 @@
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
          $('[rel="icon"]').attr('href', "/images/favicon-32x32.ico");
-         document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
+         document.title = '(っェ⊂)我藏好了哦~';
          clearTimeout(titleTime);
      }
      else {
          $('[rel="icon"]').attr('href', "/images/favicon-32x32.ico");
-         document.title = '(ฅ>ω<*ฅ) 噫又好了~' + OriginTitle;
+         document.title = 'o(^▽^)o被你发现啦~' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
